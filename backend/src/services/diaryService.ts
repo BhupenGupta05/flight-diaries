4bcde5c92afc019faab41b77256f3604ab1cde89
Bhupen Gupta <bhupengupta05@gmail.com>
2024-03-15 12:14:41 +0530

    POST requests done

--- backend/src/services/diaryService.ts
+++ backend/src/services/diaryService.ts
@@ -1,5 +1,5 @@
 import diaries from '../../data/entries'
-import { NonSensitiveDiaryEntry, DiaryEntry } from '../types'
+import { NewDiaryEntry, NonSensitiveDiaryEntry, DiaryEntry } from '../types'
 
 const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
     return diaries.map(({id, date, weather, visibility}) => ({
@@ -10,16 +10,27 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
     }))
 }
 
+const findById = (id: number): DiaryEntry | undefined => {
+    const entry = diaries.find(d => d.id === id)
+    return entry
+}
+
 const getEntries = (): DiaryEntry[] => {
     return diaries
 }
 
-const addDiary = () => {
-    return null
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+    const newDiaryEnrty = {
+        id: Math.max(...diaries.map(d => d.id)) + 1,
+        ...entry
+    }
+    diaries.push(newDiaryEnrty)
+    return newDiaryEnrty
 }
 
 export default {
     getEntries, 
+    findById,
     getNonSensitiveEntries,
     addDiary
 }
