6b36047ac13fbf864b2c6500eae022dd4d70dea9
Bhupen Gupta <bhupengupta05@gmail.com>
2024-03-15 12:56:47 +0530

    Prsing data for POST requests

--- backend/src/routes/diaries.ts
+++ backend/src/routes/diaries.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import diaryService from '../services/diaryService'
+import toNewDiaryEntry from '../utils';
 
 const router = express.Router()
 
@@ -18,14 +19,19 @@ router.get('/:id', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    const {date, weather, visibility, comment} = req.body
-    const addedEntry = diaryService.addDiary({
-        date,
-        weather, 
-        visibility,
-        comment
-    })
-    res.json(addedEntry)
+    try {
+        const newDiaryEnrty = toNewDiaryEntry(req.body)
+
+        const addedEntry = diaryService.addDiary(newDiaryEnrty)
+        res.json(addedEntry)
+
+    } catch (error: unknown) {
+        let errorMessage = 'Something went wrong.'
+        if(error instanceof Error) {
+            errorMessage += 'Error: ' + error.message
+        }
+        res.status(400).send(errorMessage)
+    }
 })
 
 export default router
